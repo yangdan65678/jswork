c15930d5cbd6dd31964b0370f251ea24dec266d5
apple <apple@jd505-50.local>
2019-10-12 13:01:07 +0800

    work4

--- work4/work4.js
+++ work4/work4.js
@@ -5,11 +5,11 @@ if(isNaN(level)){
     alert('金字塔的层数是数字')
 }
 for(let i=1;i<=level;++i){
-    let blank = level -i;
-    for(let k=o;k<blank;++kk){
-        document.write('*')
+    let blank = level - i;
+    for(let k=o;k<blank;++k){
+        document.write('&nbsp;');
     }
-    let star =i * 2 -1
+    let star =i * 2 - 1
     for(let j=0;j<star;++j){
         document.write('*')
     }
@@ -19,11 +19,11 @@ for(let i=1;i<=level;++i){
 let str = '<table border="1">'
 for(let i=1;i<10;++i){
     str +='<tr>' 
-    for(var i=1;i<10;++i){
-        str +='<td>'+ j + '*' + i +'='+( j * i) + '<td>'
+    for(var j=1;j<=i;++j){
+        str +='<td>' + j + '*' + i +'='+( j * i) + '<td>'
 
 }
 str +='<tr>'
 }
-str +='<table>'
+str +='</table>'
 document.getElementById('table').innerHTML =str
