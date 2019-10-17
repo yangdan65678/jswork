497e50b01bec8ba4b3ceb09f8d14ff7451be397b
yangdan65678 <2397300735@qq.com>
2019-10-17 10:01:12 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,31 @@
+function calculate(){
+    let num = document.getElementById('').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('').value=pi(num)
+    }
+}
+function factorial(n){
+    let sum =1
+    for(let i=1;i<=){
+
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum=1
+    for(let i=1;i<=n;++i)
+    
+}
+return sum
+}
+function pi(num){
+    let sum =0
+    for(let i=0;i<=num;++i)
+    let dividend =factorial()
+    let divisor = oddFactorial()
+
+}
+return
+}
+calculate()
