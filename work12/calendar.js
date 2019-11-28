d2561fcdad9aa7754bb24aac1e37f3eb3336e40c
yangdan65678 <2397300735@qq.com>
2019-11-28 22:09:52 +0800

    work13

--- work12/calendar.js
+++ work12/calendar.js
@@ -3,8 +3,8 @@ function calendar(y){
     var html ='<div class="box">';
     for(var m =1;m<=12;++m){
         html +='<table>';
-        html +='<tr class="title"><th colspan="7">' +y +'年' +m +'月</th></tr>';
-        html +='<tr><td>日</td><td>-</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
+        html +='<tr class="title"><th colspan="7">' + y +'年' +m +'月</th></tr>';
+        html +='<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td></tr>';
         var max = new Data(y,m,0).getDate();
         html +='<tr>';
         for(var d=1;d<= max;++d){
--- work12/index.js
+++ work12/index.js
@@ -1,5 +1,10 @@
 let year = new Date().getFullYear()
 document.getElementById("cal").innerHTML =calender(year)
 function setCalender(year){
+    //设置preyear
     
+    //设置curyear
+    //设置nextyear
+    //设置cal显示年历
+      preyear
 }
